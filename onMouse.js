15dab365f0de76faf58e09937f4feb7f985b2e6c
tianaFomicheva <tianirika@gmail.com>
2021-02-08 00:11:52 +0500

    правильный расчет для шарика

--- onMouse.js
+++ onMouse.js
@@ -6,10 +6,11 @@
     var follower = $("#flying");
     var offset = $('#flying').offset()
     var xp = offset.left
-    var yp = offset.top
+    var yp = offset.top 
     var loop = setInterval(function(){
-    xp += (mouseX - xp - 100) / 40;
-    yp += (mouseY - yp - 60) / 40;
-    follower.css({left:xp, top:yp});
+    xp += (mouseX - xp - 30) / 70;
+    yp += (mouseY - yp - 30 ) / 70;
+
+    follower.offset({left:xp, top:yp});
  }, 10);
     })
